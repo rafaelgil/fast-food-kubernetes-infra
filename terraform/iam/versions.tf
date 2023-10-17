6b5e12b896bebc642d7873f397f157fc0e7a2164
Rafael Carmona Gil <rafaelcarmonagil@gmail.com>
2023-10-16 21:26:07 -0300

    Criando as roles

--- terraform/iam/versions.tf
+++ terraform/iam/versions.tf
@@ -1,13 +1,13 @@
-terraform {
+#terraform {
   #   required_version = ">= 4.48.0"
-  backend "s3" {
-    bucket = "aws-terraform-demo"
-    key    = "dev/eks-with-tf/iam-roles.tfstate"
-    region = "us-east-1"
+#  backend "s3" {
+#    bucket = "aws-terraform-demo"
+#    key    = "dev/eks-with-tf/iam-roles.tfstate"
+#    region = "us-east-1"
 
-    dynamodb_table = "vpc-solution"
-  }
-}
+#    dynamodb_table = "vpc-solution"
+#  }
+#}
 
 provider "aws" {
   region = var.aws_region
