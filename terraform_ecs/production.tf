9015c71cef7831a4d4db6f998eadb94713a8bddc
Rafael Carmona Gil <rafaelcarmonagil@gmail.com>
2023-10-19 08:48:42 -0300

    Teste ECS

---
+++ terraform_ecs/production.tf
@@ -0,0 +1,24 @@
+
+locals {
+  production_availability_zones = ["us-east-1a", "us-east-1b"]
+}
+
+provider "aws" {
+  region  = "${var.region}"
+}
+
+resource "aws_key_pair" "key" {
+  key_name   = "production_key"
+  public_key = "${file("production_key.pub")}"
+}
+
+module "networking" {
+  source               = "./modules/networking"
+  environment          = "production"
+  vpc_cidr             = "10.0.0.0/16"
+  public_subnets_cidr  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets_cidr = ["10.0.10.0/24", "10.0.20.0/24"]
+  region               = "${var.region}"
+  availability_zones   = "${local.production_availability_zones}"
+  key_name             = "production_key"
+}
---
+++ terraform_ecs/variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  description = "Region that the instances will be created"
+}
+
+variable "production_database_name" {
+  description = "The database name for Production"
+}
+
+variable "production_database_username" {
+  description = "The username for the Production database"
+}
+
+variable "production_database_password" {
+  description = "The user password for the Production database"
+}
+
+variable "production_secret_key_base" {
+  description = "The Rails secret key for production"
+}
