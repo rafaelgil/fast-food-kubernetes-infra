138e707ac0e2491592a1558b9c0a901804645bac
Rafael Carmona Gil <rafaelcarmonagil@gmail.com>
2023-10-22 12:43:34 -0300

    Delete EKS

--- terraform/environments/dev/deploy-bastion-host.tfvars
+++
@@ -1,4 +0,0 @@
-aws_region    = "us-east-1"
-instance_type = "t3.micro"
-instance_user = "ec2-user"
-key_pair_name = "demokey1"
--- terraform/iam/main.tf
+++
@@ -1,6 +0,0 @@
-module "iam-role" {
-  source = "../modules/iam-role"
-
-  cluster_roles   = var.cluster_roles
-  nodegroup_roles = var.nodegroup_roles
-}
